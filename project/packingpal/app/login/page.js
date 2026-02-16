afcdf78a9fa831b1aef064c6fcf936a71403dd5e
MEB <122181156+moniesco22@users.noreply.github.com>
2026-02-16 15:33:13 -0500

    sign up updates
    Sign up page finally shows

--- project/packingpal/app/login/page.js
+++ project/packingpal/app/login/page.js
@@ -70,7 +70,7 @@ export default function LoginPage() {
           </p>
             <p className="mt-4 text-sm text-center text-slate-600">
               Don’t have an account?{" "}
-              <Link href="/signup" className="text-blue-600 hover:underline">
+              <Link href="/sign_up" className="text-blue-600 hover:underline">
                  Sign up
               </Link>
             </p>
